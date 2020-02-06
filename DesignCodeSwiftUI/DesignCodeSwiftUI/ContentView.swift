a0b96402625bbfcb0abd8d107568e9f309525ec0
viniciuscarvalho <viniciuscarvalho789@gmail.com>
2020-02-05 21:52:58 -0300

    Commit initial

---
+++ DesignCodeSwiftUI/DesignCodeSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCodeSwiftUI
+//
+//  Created by Vinicius Carvalho Marques on 05/02/20.
+//  Copyright © 2020 Vinicius Carvalho. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
