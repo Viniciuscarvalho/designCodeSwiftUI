209f1378090434e50774650c2c745a11c5035618
viniciuscarvalho <viniciuscarvalho789@gmail.com>
2020-04-02 19:10:31 -0300

    Add combine for scaffold on project

---
+++ DesignCodeSwiftUI/DesignCodeSwiftUI/UpdateDetail.swift
@@ -0,0 +1,21 @@
+//
+//  UpdateDetail.swift
+//  DesignCodeSwiftUI
+//
+//  Created by Vinicius Carvalho Marques on 02/04/20.
+//  Copyright © 2020 Vinicius Carvalho. All rights reserved.
+//
+
+import SwiftUI
+
+struct UpdateDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UpdateDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        UpdateDetail()
+    }
+}
---
+++ DesignCodeSwiftUI/DesignCodeSwiftUI/UpdateStore.swift
@@ -0,0 +1,21 @@
+//
+//  UpdateStore.swift
+//  DesignCodeSwiftUI
+//
+//  Created by Vinicius Carvalho Marques on 02/04/20.
+//  Copyright © 2020 Vinicius Carvalho. All rights reserved.
+//
+
+import SwiftUI
+
+struct UpdateStore: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UpdateStore_Previews: PreviewProvider {
+    static var previews: some View {
+        UpdateStore()
+    }
+}
